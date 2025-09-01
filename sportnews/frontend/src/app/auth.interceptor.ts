c7d3deb6e9f17a484578351097508eb10dded9bf
ThetaLog(n) <giorgiomartucci02@gmail.com>
2025-09-01 16:38:52 +0200

    update

--- sportnews/frontend/src/app/auth.interceptor.ts
+++ sportnews/frontend/src/app/auth.interceptor.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, NgZone } from '@angular/core';
 import {
   HttpInterceptor,
   HttpRequest,
@@ -13,17 +13,19 @@ import { AuthService } from '@auth0/auth0-angular';
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor(private auth: AuthService) {}
+  constructor(private auth: AuthService, private ngZone: NgZone) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req).pipe(
       catchError((error: HttpErrorResponse) => {
         if (error.status === 401) {
-          // Popup semplice (puoi sostituire con snackbar Angular Material)
-          alert('⚠️ Sessione scaduta o non autenticato. Verrai reindirizzato al login.');
+          this.ngZone.run(() => {
+            // Popup
+            alert('⚠️ Sessione scaduta o non autenticato. Verrai reindirizzato al login.');
 
-          // Reindirizza a login
-          this.auth.loginWithRedirect();
+            // Redirect
+            this.auth.loginWithRedirect();
+          });
         }
         return throwError(() => error);
       })
