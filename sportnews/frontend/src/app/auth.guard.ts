18e847c618b125e9617fa987d73be5b928b8e806
ThetaLog(n) <giorgiomartucci02@gmail.com>
2025-09-06 22:47:26 +0200

    login

--- sportnews/frontend/src/app/auth.guard.ts
+++ sportnews/frontend/src/app/auth.guard.ts
@@ -1,5 +1,25 @@
-import { CanActivateFn } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { AuthService } from '@auth0/auth0-angular';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
-export const authGuard: CanActivateFn = (route, state) => {
-  return true;
-};
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private auth: AuthService, private router: Router) {}
+
+  canActivate(): Observable<boolean> {
+    return this.auth.isAuthenticated$.pipe(
+      map(isAuthenticated => {
+        if (isAuthenticated) {
+          return true;
+        } else {
+          return false;
+        }
+      })
+    );
+  }
+}
